45d57f029295df4855650cb9c715e0564a979dd6
Tyler Hancock <darklime@live.ca>
2023-05-19 14:47:23 -0600

    Port to MC 1.19.2

--- Common/src/main/java/net/darkhax/thirstybottles/mixin/MixinBottleItem.java
+++ Common/src/main/java/net/darkhax/thirstybottles/mixin/MixinBottleItem.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Mixin(BottleItem.class)
 public class MixinBottleItem {
 
-    @Inject(method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;", at = @At(ordinal = 1, value = "INVOKE", target = "Lnet/minecraft/world/level/Level;gameEvent(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/core/BlockPos;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    @Inject(method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;gameEvent(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/core/BlockPos;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void onItemUsed(Level worldLevel, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cbi, List effects, ItemStack heldStack, HitResult clickInfo, BlockPos clickPos) {
         if (!worldLevel.isClientSide) {
             worldLevel.setBlock(clickPos, Blocks.AIR.defaultBlockState(), 2);
