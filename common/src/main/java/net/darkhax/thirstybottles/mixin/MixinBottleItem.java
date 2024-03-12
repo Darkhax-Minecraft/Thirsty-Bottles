b4079c7df584117e2c6e8255ae1c49491865378a
Tyler Hancock <darklime@live.ca>
2024-03-12 17:30:27 -0600

    Fix neighboring non-source blocks not being updated.

--- common/src/main/java/net/darkhax/thirstybottles/mixin/MixinBottleItem.java
+++ common/src/main/java/net/darkhax/thirstybottles/mixin/MixinBottleItem.java
@@ -24,7 +24,7 @@ public class MixinBottleItem {
     @Inject(method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;gameEvent(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/level/gameevent/GameEvent;Lnet/minecraft/core/BlockPos;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void onItemUsed(Level worldLevel, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cbi, List effects, ItemStack heldStack, BlockHitResult clickInfo, BlockPos clickPos) {
         if (!worldLevel.isClientSide) {
-            worldLevel.setBlock(clickPos, Blocks.AIR.defaultBlockState(), 2);
+            worldLevel.setBlock(clickPos, Blocks.AIR.defaultBlockState(), 3);
         }
     }
 }
